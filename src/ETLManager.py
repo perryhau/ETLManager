4d257b425fa44ec94959615107520c44948d96b5
dengar <d3ngar@gmail.com>
2013-05-06 00:26:19 +0100

    nearly at earlier version

--- src/ETLManager.py
+++ src/ETLManager.py
@@ -2,15 +2,48 @@
 
 import Tkinter as tk
 import menu_bar
+import importWindow as iw
 
 class ETLManager():
+    
+    def append_to_log(self, text):
+        self.text_area.config(state = "normal")
+        self.text_area.insert("end", text + '\n')
+        self.text_area.config(state = "disabled")
+    
+    def b_import(self):
+        imp = iw.import_window(self)
+        self.append_to_log("import selected")
     
     def initialize(self):
         self.main_win = tk.Tk()
         self.main_win.title("ETL Manager")
-        self.main_win.geometry("550x350+200+200")
+        self.main_win.geometry("500x370+200+200")
         
-        self.main_win.config(menu = menu_bar.ret_menu_bar())
+        ## Menu Bar
+        mb = tk.Menu(self.main_win)
+        menu_bar.ret_menu_bar(mb)
+        self.main_win.config(menu = mb)
+        
+        ## Other stuff
+        self.import_button = tk.Button(self.main_win, text="Import...", command = self.b_import, height = 5, width=15)
+        self.im_schedule_button = tk.Button(self.main_win, text="Import Schedule...", command = self.b_import, height = 5, width=15)
+        self.report_button = tk.Button(self.main_win, text="Reporting...", command = self.b_import, height = 5, width=15)
+        
+        self.online_button = tk.Button(self.main_win, text="Internet...", command = self.b_import, height = 5, width=15)
+        self.extract_button = tk.Button(self.main_win, text="Extract...", command = self.b_import, height = 5, width=15)
+        self.ex_schedule_button = tk.Button(self.main_win, text="Export Schedule...", command = self.b_import, height = 5, width=15)
+        
+        self.text_area = tk.Text(self.main_win, height = 12, width = 63, bg="black", fg="green")
+        
+        self.import_button.grid(row = 0, column = 0, sticky="nw")
+        self.im_schedule_button.grid(row = 0, column = 1, sticky="nw")
+        self.report_button.grid(row = 0, column = 2, sticky="nw")
+        self.online_button.grid(row = 1, column = 0, sticky="nw")
+        self.extract_button.grid(row = 1, column = 1, sticky="nw")
+        self.ex_schedule_button.grid(row = 1, column = 2, sticky="nw")
+        self.text_area.grid(row = 2, column = 0, columnspan = 3, sticky="nw")
+        
     
     def __init__(self, parent):
         self.parent = parent
--- src/menu_bar.py
+++ src/menu_bar.py
@@ -1,25 +1,49 @@
 #! /usr/bin/env python
 
 from Tkinter import *
+import tkMessageBox
+import tkFileDialog
+import time
+import sys
 
-def ret_menu_bar():
-    menu_bar = Menu()
+def menu_open():
+    #do nothing
+    file_name = tkFileDialog.askopenfile()
+    print file_name 
+    #time.sleep(10)
+    return
+    
+def menu_about():
+    tkMessageBox.showinfo(title="ETL Manager About", message="This is an early version of an ETL Manager")
+    #tkMessageBox.showerror(title="ETL Manager About", message="This is an early version of an ETL Manager")
+    #tkMessageBox.showwarning(title="ETL Manager About", message="This is an early version of an ETL Manager")
+    return
+    
+def menu_quit():
+    check_exit = tkMessageBox.askyesno(title = "Quit?", message = "Do you really want to close?")
+    if check_exit > 0:
+        print "Closing the programme"
+        quit(0)
+    return
+
+def ret_menu_bar(menu_bar):
     
     ## File Menu
     file_menu = Menu(menu_bar, tearoff = 0)
     
     file_menu.add_command(label = "New...")
-    file_menu.add_command(label = "Open...")
+    file_menu.add_command(label = "Open...", command = menu_open)
     file_menu.add_command(label = "Save")
     file_menu.add_command(label = "Save As...")
-    file_menu.add_command(label = "Close...")
+    file_menu.add_separator()
+    file_menu.add_command(label = "Close...", command = menu_quit)
     
     menu_bar.add_cascade(label = "File", menu = file_menu)
     
     ## Settings Menu
     settings_menu = Menu(menu_bar, tearoff = 0)
     
-    settings_menu.add_command(label = "Checkable")
+    settings_menu.add_checkbutton(label = "Checkable")
     settings_menu.add_command(label = "Properties")
     
     menu_bar.add_cascade(label = "Settings", menu = settings_menu)
@@ -29,8 +53,7 @@ def ret_menu_bar():
     
     help_menu.add_command(label = "Help")
     help_menu.add_command(label = "Online Help...")
-    help_menu.add_command(label = "About")
+    help_menu.add_command(label = "About", command = menu_about)
     
     menu_bar.add_cascade(label = "Help", menu = help_menu)
     
-    return
