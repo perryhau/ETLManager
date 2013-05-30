891e160db3cf304cf401ee32f7014debcaaf6133
dengar <dengar@dengar-net>
2013-05-31 00:31:02 +0100

    changes to the File Importer (progress) Edits to the File Reader. Probably needs some re-writing.

--- src/FileImporter.py
+++ src/FileImporter.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 class importFile():
     
-    def __init__(self):
+    def __init__(self, inc_header = True, start_line = 0, file_del = "auto"):
         s = settings_parser()
         settings = s.initialize("../chris_home.xml")
         
@@ -16,9 +16,28 @@ class importFile():
             db_conn = db_connection(self, settings['connection'], settings['user'], settings['password'], settings['db_name'])
         else:
             print "not recognised connection type"
+        
+        if file_del <> "excel" and file_del <> "xml":
+        
+            fr = FileReader("../testImportFile.txt")
+            if file_del == "auto":
+                file_del = fr.discover_delimiter(starting_line = start_line)
             
-        fr = FileReader("../testImportFile.txt")
-        fr.discover_delimiter()
+            if file_del == "comma" :
+                delimit = ","
+            if file_del == "tab":
+                delimit = "\t"
+            if file_del == "semi":
+                delimit = ";"
+            if file_del == "pipe":
+                delimit = "|"
+            
+            if inc_header == True :
+                header_line = start_line
+                header = fr.get_line(header_line, delimit)
+                print header
+                for el in header:
+                    print el.strip()
     
 if __name__ == "__main__":
     
@@ -26,6 +45,7 @@ if __name__ == "__main__":
     delimiter = 'auto'
     tbl_type = 'new'
     start_line = 0
+    quote_char = ""
     
     date_time = datetime.now()
     dtm = datetime.strftime(date_time, "%Y%m%d_%H%M%S")
@@ -35,12 +55,18 @@ if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option("-c", "--column_headers", dest="ch", help="Set True or False for column headers. Default is False", default=col_head)
     parser.add_option("-s", "--starting_line", dest="sl", help="What line of the file to start reading it. Default is 0.", default=col_head)
-    parser.add_option("-d", "--delimiter", dest="d", help="File delimiter option (comma, pipe, semicolon, tab, excel, xml). Default is auto discover", default=delimiter)
-    parser.add_option("-tt", "--table_option", dest="tt", help="Is the destination a new or existing table. Default is new.", default=tbl_type)
-    parser.add_option("-tn", "--table_name", dest="tn", help="Destination table name. Default is temp_YYYYMMDD_HHMMSS.", default=tbl_name)
+    parser.add_option("-d", "--delimiter", dest="d", help="File delimiter option (comma, pipe, semicolon, tab, excel, xml). Default is auto discover from plain text.", default=delimiter)
+    parser.add_option("-t", "--table_option", dest="tt", help="Is the destination a new or existing table. Default is new.", default=tbl_type)
+    parser.add_option("-n", "--table_name", dest="tn", help="Destination table name. Default is temp_YYYYMMDD_HHMMSS.", default=tbl_name)
+    parser.add_option("-q", "--quote_char", dest="qc", help="The Quote Character. Usually is \" if the file uses one. The default value is not to utilise a quote char", default=quote_char)
     
     (args, values) = parser.parse_args()
     
-    start_date = args.start_date
+    col_head = args.ch
+    delimiter = args.d
+    tbl_type = args.tt
+    start_line = args.sl
+    tbl_name = args.tn
+    quote_char = args.qc
     
     app = importFile()
--- src/FileReader.py
+++ src/FileReader.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 #FileReader module and class. 
 
+import csv
+
 class FileReader:
     file1 = 'filename'
     
@@ -9,64 +11,75 @@ class FileReader:
         self.file1 = filename
         #print filename
         
-    def discover_delimiter(self):
+    def get_line(self, line, delimit):
+        fo = open(self.file1, 'r', 1)
+        lines = fo.readlines()
+        arr = lines[line].strip().split(delimit)
+        return arr
+    
+    def discover_delimiter(self, starting_line = 0):
         foundDel = ''
         max_dels = {'comma':0, 'tab':0, 'pipe':0, 'semi':0}
         num_lines = 0
         
         with open(self.file1) as infp:
+            t = 0
             for line in infp:
-                num_lines += 1
+                if t >= starting_line:
+                    num_lines += 1
+                t += 1
                 
         if num_lines > 500:
             num_lines = 500
-        
-        #print num_lines
         
         fo = open(self.file1, 'r', 1)
         lines = fo.readlines()
         
         for i in range(num_lines):
-            arr = lines[i].split(',')
+            arr = lines[i+starting_line].split(',')
             if max_dels['comma'] < len(arr):
                 max_dels['comma'] = len(arr)
         
         for i in range(num_lines):
-            arr = lines[i].split('\t')
+            arr = lines[i+starting_line].split('\t')
             if max_dels['tab'] < len(arr):
                 max_dels['tab'] = len(arr)
                 
         for i in range(num_lines):
-            arr = lines[i].split('|')
+            arr = lines[i+starting_line].split('|')
             if max_dels['pipe'] < len(arr):
                 max_dels['pipe'] = len(arr)
         
         for i in range(num_lines):
-            arr = lines[i].split(';')
+            arr = lines[i+starting_line].split(';')
             if max_dels['semi'] < len(arr):
                 max_dels['semi'] = len(arr)
         
         foundDel = max(max_dels, key=max_dels.get)
-        print foundDel
+        print "The delimiter found is: " + foundDel
+        return foundDel
 
     def readTextFile (self):
         fo = open(self.file1, 'r', 1)
         l = []
         for line in fo.readlines():
-            line = line.rstrip()
+            line = line.strip()
             if len(line) > 0:
                 l.append(line)
         fo.close()
         return l
     
-    def readTextToArray (self, delimit):
+    def readTextToArray (self, delimit, starting_line = 0, quote_char = None):
         fo = open(self.file1, 'r', 1)
         arr = []
+        i = 0
         
         for line in fo.readlines():
-            line = line.rstrip()
-            if len(line) > 0:
-                arr.append(line.split(delimit))
+            if i >= starting_line:
+                line = line.strip()
+                if len(line) > 0:
+                    arr.append(line.split(delimit))
+            i += 1
             
         fo.close()
         return arr
@@ -75,7 +88,7 @@ class FileReader:
         fo = open(self.file1, 'r', 1)
         dic = {}
         for line in fo.readlines():
-            line = line.rstrip()
+            line = line.strip()
             arr = line.split(delimit)
             if len(line) > 0:
                 dic[arr[0]] = arr[1]
