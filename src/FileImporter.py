fe62a578dc9908c09355b05dc74ff7f2d1765ab1
dengar <dengar@dengar-net>
2013-06-14 17:10:35 +0100

    some cleaning up. new test import - working fine so far...

--- src/FileImporter.py
+++ src/FileImporter.py
@@ -25,9 +25,11 @@ class importFile():
                 fr = FileReader(file_name)
                 
                 if file_del == "auto":
+                    print "trying to discover file delimter"
                     file_del = fr.discover_delimiter(starting_line = start_line)
             
-                if inc_header == True :
+                if inc_header == True:
+                    print "getting column headers from file"
                     header_line = start_line
                     header = fr.get_line(header_line, file_del)
                     arr = []
@@ -36,23 +38,22 @@ class importFile():
                         arr.append(st01)
                     header = arr
                 
-                if inc_header == True : 
+                print "reading content"
+                if inc_header == True:
                     content = fr.readTextToArrayList(file_del, st_line = start_line+1, skip_end_lines = skip_last)
                 else:
                     content = fr.readTextToArrayList(file_del, st_line = start_line, skip_end_lines = skip_last)
-
-                #print content
                 
                 
                 # create table if needed
                 if tbl_type == "new":
+                    print "creating new table"
                     if inc_header == True:
                         tbl_cr = table_creator(content, tbl_name, header = header)
                     else:
                         tbl_cr = table_creator(content, tbl_name)
                     
                     new_tbl_stmt = tbl_cr.return_newTableStmt()
-                    #print new_tbl_stmt
                     db_conn.cursor.execute(new_tbl_stmt)
                     db_conn.con.commit()
                     print "table %s created!" % tbl_name
@@ -60,8 +61,12 @@ class importFile():
                     header = tbl_cr.return_header()
                 
                 ## try inserting the lines
+                print "inserting data"
+                counter = 1
                 for line in content:
-                    
+                    #Say something every 100 lines:
+                    if counter % 100 == 0:
+                        print "Done %(line_count)i lines" % {'line_count':counter}
                     ### prepare the insert statement for the line
                     i = 0
                     ins_stmt = "insert into " + tbl_name + " ("
@@ -79,14 +84,13 @@ class importFile():
                     
                     ### insert the line:
                     try:
-                        #print ins_stmt
-                        #print list(line)
                         db_conn.cursor.execute(ins_stmt, list(line))
-                        db_conn.con.commit()
-                        pass
                     except:
-                        print "This didn't work"
-                                        
+                        print "This didn't work, error in line %(line_count)i" % {'line_count':counter}
+                    
+                    counter += 1
+                db_conn.con.commit()
+                            
                 ## logging the import job
             
             #closing the db:
@@ -95,13 +99,15 @@ class importFile():
     
 if __name__ == "__main__":
     
-    col_head = False
+    col_head = True
     delimiter = 'auto'
     tbl_type = 'new'
-    start_line = 0
+    sLine = 0
     quote_char = ""
-    settings_file = "../default.xml"
+    setfile = "../chris_home.xml"
     file_name = "../testImportFile.txt"
+    skip_end_no = 0
+    final_line = None
     
     date_time = datetime.now()
     dtm = datetime.strftime(date_time, "%Y%m%d_%H%M%S")
@@ -110,23 +116,29 @@ if __name__ == "__main__":
     
     parser = OptionParser()
     parser.add_option("-f", "--import_file", dest="f", help="What file should be used for this import. This setting is MANDATORY. Use -f FILE_NAME to use", default=file_name)
-    parser.add_option("-S", "--settings_file", dest="set", help="Loading a different database connection file. Default is ../chris_home.xml", default=settings_file)
+    parser.add_option("-S", "--settings_file", dest="set", help="Loading a different database connection file. Default is ../chris_home.xml", default=setfile)
     parser.add_option("-c", "--column_headers", dest="ch", help="Set True or False for column headers. Default is False", default=col_head)
-    parser.add_option("-s", "--starting_line", dest="sl", help="What line of the file to start reading it. Default is 0.", default=col_head)
+    parser.add_option("-s", "--starting_line", dest="sl", help="What line of the file to start reading it. Default is 0.", default=sLine)
     parser.add_option("-d", "--delimiter", dest="d", help="File delimiter option (comma, pipe, semicolon, tab, excel, xml). Default is auto discover from plain text.", default=delimiter)
     parser.add_option("-t", "--table_option", dest="tt", help="Is the destination a new or existing table. Default is new.", default=tbl_type)
     parser.add_option("-n", "--table_name", dest="tn", help="Destination table name. Default is temp_YYYYMMDD_HHMMSS.", default=tbl_name)
     parser.add_option("-q", "--quote_char", dest="qc", help="The Quote Character. Usually is \" if the file uses one. The default value is not to utilise a quote char", default=quote_char)
+    parser.add_option("-e", "--skip_lines_end", dest="skl", help="Skip a number of lines at the end of the file. Can be useful if there is a summary of some sort at the bottom of a report. The default is 0", default=skip_end_no)
+    parser.add_option("-E", "--end_line", dest="el", help="If there is only a certain number of lines to import, use this to specify the end line. Normally 'None'", default=final_line)
     
     (args, values) = parser.parse_args()
     
     col_head = args.ch
     delimiter = args.d
     tbl_type = args.tt
-    start_line = args.sl
-    tbl_name = args.tn
+    sLine = args.sl
+    #tbl_name = args.tn
+    tbl_name = "test_table01"
     quote_char = args.qc
     file_name = args.f
-    settings_file = args.set
+    setfile = args.set
+    skip_end_no = args.skl
+    final_line = args.el
     
-    app = importFile(file_name, tbl_name, tbl_type)
+    print "starting the file import" 
+    app = importFile(file_name, tbl_name, tbl_type, settings_file = setfile, inc_header = col_head, start_line = sLine, file_del = delimiter, end_line = final_line, skip_last = skip_end_no)
